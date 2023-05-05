0d0a27a60fbc6a89f211f911c92b599b1bb13d7a
Sudhansu <13695177+the-lazy-learner@users.noreply.github.com>
2023-05-05 12:58:52 +0530

    Modify: Localize scope of color (#1)
    Co-authored-by: Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.co>

--- visualize-penguins.R
+++ visualize-penguins.R
@@ -7,3 +7,8 @@ ggplot(
   data = penguins,
   mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species),
 ) + geom_point()
+
+ggplot(data = penguins,
+       mapping = aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(mapping = aes(color = species)) +
+  geom_line('lm')
